e693b28c77ccfe4d84630b666411122aa9f542df
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-10-21 19:45:13 +0700

    done: login using localhost test api

--- app/src/main/java/com/hexademical/awayfromusapplication/LoginActivity.kt
+++ app/src/main/java/com/hexademical/awayfromusapplication/LoginActivity.kt
@@ -3,7 +3,14 @@ package com.hexademical.awayfromusapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.hexademical.awayfromusapplication.API.UserRequest
+import com.hexademical.awayfromusapplication.API.UserRespone
+import com.hexademical.awayfromusapplication.Interface.UserApi
+import com.hexademical.awayfromusapplication.Retrofit.Retro
 import com.hexademical.awayfromusapplication.databinding.ActivityLoginBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
     // @ binding
@@ -29,11 +36,34 @@ class LoginActivity : AppCompatActivity() {
         // login handler
         binding?.loginBtn?.setOnClickListener {
             // get username and password
-            val username = binding.lUsername.text.toString()
-            val password = binding.lPassword.text.toString()
+            val username = binding.lUsername.text.toString().trim()
+            val password = binding.lPassword.text.toString().trim()
             Log.d(TAG, "username: ${username} password: ${password}")
+            login(username, password)
         }
 
+    }
+
+
+    fun login(_username: String, _password: String) {
+        val request = UserRequest()
+        request.username = _username
+        request.password = _password
+        val retro = Retro().getRetroClientInstance().create(UserApi::class.java)
+        retro.login(request).enqueue(object : Callback<UserRespone>{
+            override fun onResponse(call: Call<UserRespone>?, response: Response<UserRespone>?) {
+                val user = response?.body()
+                Log.d(TAG, "Token: ${user!!.data?.token}")
+                Log.d(TAG, "Username: ${user!!.data?.username}")
+                Log.d(TAG, "Firstname: ${user!!.data?.firstname}")
+
+            }
+
+            override fun onFailure(call: Call<UserRespone>?, t: Throwable?) {
+                Log.e(TAG, "error")
+            }
+
+        })
     }
 
 
