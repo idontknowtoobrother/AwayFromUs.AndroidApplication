35bdb70a23d932b6a307c02e598fbfa683e4e87d
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-10-21 18:11:59 +0700

    view binding login and user management

--- app/src/main/java/com/hexademical/awayfromusapplication/LoginActivity.kt
+++ app/src/main/java/com/hexademical/awayfromusapplication/LoginActivity.kt
@@ -2,10 +2,17 @@ package com.hexademical.awayfromusapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hexademical.awayfromusapplication.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    // @ binding
+    private lateinit var  binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        // binding inflate
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // now you not want to use R.findViewById anymore
     }
 }
--- app/src/main/java/com/hexademical/awayfromusapplication/UserManagementActivity.kt
+++ app/src/main/java/com/hexademical/awayfromusapplication/UserManagementActivity.kt
@@ -2,10 +2,17 @@ package com.hexademical.awayfromusapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hexademical.awayfromusapplication.databinding.ActivityUserManagementBinding
 
 class UserManagementActivity : AppCompatActivity() {
+    // @ binding
+    private lateinit var binding: ActivityUserManagementBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_management)
+        // binding inflate
+        binding = ActivityUserManagementBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // now you not want to use R.findViewById anymore
     }
 }
